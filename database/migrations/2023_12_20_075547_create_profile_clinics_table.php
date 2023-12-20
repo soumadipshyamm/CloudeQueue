af6fdb52349a884abb2912058ac0ffec55b35b99
Hazra Soumadip <soumadip.hazra@shyamfuture.com>
2023-12-20 14:50:38 +0530

    profile

---
+++ database/migrations/2023_12_20_075547_create_profile_clinics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_clinics', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique()->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('clinic_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('lat')->nullable();
+            $table->string('long')->nullable();
+            $table->time('time')->nullable();
+            $table->string('description')->nullable();
+            $table->tinyInteger('is_active')->default(true)->comment('0:Inactive,1:Active')->nullable();
+            $table->string('profile_images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile_clinics');
+    }
+};
